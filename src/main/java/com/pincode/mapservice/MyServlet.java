86e4172276d2c3145ad956afc1519d1500ce7c0a
ssn2013 <sruthi.nair@Sruthi-Nair.local>
2013-08-01 21:11:02 +0530

    pincode ajax request

---
+++ src/main/java/com/pincode/mapservice/MyServlet.java
@@ -0,0 +1,11 @@
+package com.pincode.mapservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sruthi.nair
+ * Date: 8/1/13
+ * Time: 8:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyServlet {
+}
